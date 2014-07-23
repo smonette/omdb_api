6ae075140d39fc63092680a9e592a861b1c22f8e
smonette <steph@stephmonette.com>
2014-07-22 18:02:54 -0700

    initial commit of lab work

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 var express = require("express");
-var request = require('request');
+var request = require("request");
+
 var app = express();
 
-// This is nessecary because express will get lazy, and only look at routes
-// It wont actually look into static files, and will return 404 on stylesheets
 app.use("/styles", express.static(__dirname + '/styles'));
+  app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.render('index.ejs');
+  res.render('site/index');
 });
 
-app.get('/search', function(req, res){
-  var query = req.query.searchTerm;
-  var url = 'http://www.omdbapi.com/?s='+query;
-  request(url, function (error, response, body) {
-    if(!error){
-      var data = JSON.parse(body);
-      res.render('results.ejs', {movieList:data.Search});
-    }
-  });
+app.get('/search', function (req, res) {
+    var query = req.query.searchTerm;
+    var url = "http://www.omdbapi.com/?s=" + query;
+    request(url, function (error, response, body) {
+        if (!error) {
+            var data = JSON.parse(body);
+            res.render("site/results", {movieList: data.Search || []});
+        }
+    });
+});
 
+app.get('/movies/:id', function (req, res) {
+    var filmId = req.params.id;
+    var url = "http://www.omdbapi.com/?i=" + filmId;
+    request(url, function (error, response, body) {
+        if (!error) {
+            var data = JSON.parse(body);
+            res.render("movies/index", {details: data || []});
+        }
+    });
 });
 
 app.listen(3000);
-
-
 
